8346d39d31c1ae8acb6c9bd95bee1523efb55d9d
KoscheyScrag <koscheyscrag@yandex.com>
2018-03-04 09:34:22 +0300

    making order added, get products by category

--- Food.Constructor.UI/src/app/food-constructor/food-constructor.component.ts
+++ Food.Constructor.UI/src/app/food-constructor/food-constructor.component.ts
@@ -1,28 +1,32 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, OnDestroy } from '@angular/core';
 import { Observable } from 'rxjs/Observable';
 import { FoodService } from '../services/food.service';
 import { ServerResponse } from '../common/ServerResponse';
 import { Product } from '../common/product';
 import { NutritionalValue } from '../common/nutritional-value';
 import { Constants } from '../common/constants';
+import { Subscription } from 'rxjs/Subscription';
+import { Order } from '../common/Order';
 
 @Component({
   selector: 'app-food-constructor',
   templateUrl: './food-constructor.component.html',
   styleUrls: ['./food-constructor.component.css']
 })
-export class FoodConstructorComponent implements OnInit {
+export class FoodConstructorComponent implements OnInit, OnDestroy {
+
   logoImg = Constants.Logo;
-  products$: Observable<any>;
+  products$: Observable<ServerResponse>;
+  productsSub: Subscription;
   products: Product[];
   indicators: NutritionalValue;
-  dishParts: string[] = [];
+  dishParts: Product[] = [];
   dishCategories: string[] = [];
   constructor(private foodService: FoodService) { }
 
   ngOnInit() {
     this.products$ = this.foodService.getProducts();
-    this.products$.subscribe((p: ServerResponse) => {
+    this.productsSub = this.products$.subscribe((p: ServerResponse) => {
       this.products = JSON.parse(p.Content);
     });
     this.indicators = {
@@ -35,6 +39,10 @@ export class FoodConstructorComponent implements OnInit {
     this.dishCategories.push('Burger');
   }
 
+  ngOnDestroy(): void {
+    this.productsSub.unsubscribe();
+  }
+
   newProductSelect(data) {
     const product: Product = data.product;
     const isSelect: boolean = data.isSelect;
@@ -44,14 +52,14 @@ export class FoodConstructorComponent implements OnInit {
           this.indicators[key] += product.NutritionalValue[key];
         }
       }
-      this.dishParts.push(product.Title);
+      this.dishParts.push(product);
     } else {
       for (const key in this.indicators) {
         if (this.indicators.hasOwnProperty(key)) {
           this.indicators[key] -= product.NutritionalValue[key];
         }
       }
-      const indexToRemove = this.dishParts.indexOf(product.Title);
+      const indexToRemove = this.dishParts.indexOf(product);
       if (indexToRemove > -1) {
         this.dishParts.splice(indexToRemove, 1);
       }
@@ -63,8 +71,23 @@ export class FoodConstructorComponent implements OnInit {
     document.getElementById('list-group-item-' + dish).classList.add('active');
     console.log(dish);
     this.products$ = this.foodService.getProductsByCategory(dish);
-    this.products$.subscribe((p: ServerResponse) => {
+    this.productsSub = this.products$.subscribe((p: ServerResponse) => {
       this.products = JSON.parse(p.Content);
     });
+  }
+
+  makeOrder() {
+    const order: Order = {
+     CompanyId: null,
+     History: null,
+     State: 0,
+     Id: null,
+     CustomerId: null,
+     GetLastState: {Key: 0, Value: ''},
+     IssuePointId: null,
+     Dishes: this.dishParts
+    };
+
+    this.foodService.createOrUpdateOrder(order);
   }
 }
--- Food.Constructor.UI/src/app/services/food.service.ts
+++ Food.Constructor.UI/src/app/services/food.service.ts
@@ -2,6 +2,8 @@ import { Injectable } from '@angular/core';
 import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Constants } from '../common/constants';
 import { Observable } from 'rxjs/Observable';
+import { ServerResponse } from '../common/ServerResponse';
+import { Order } from '../common/Order';
 
 const httpOptions = {
   headers: new HttpHeaders({ 'Content-Type': 'application/json'})
@@ -12,11 +14,15 @@ export class FoodService {
 
   constructor(private http: HttpClient) { }
 
-  getProducts(): Observable<any> {
-    return this.http.get<any>(Constants.getProducts, httpOptions);
+  getProducts(): Observable<ServerResponse> {
+    return this.http.get<ServerResponse>(Constants.getProducts, httpOptions);
   }
 
-  getProductsByCategory(category: string): Observable<any> {
-    return this.http.get<any>(Constants.getProducts + '?category=' + category, httpOptions);
+  getProductsByCategory(category: string): Observable<ServerResponse> {
+    return this.http.get<ServerResponse>(Constants.getProducts + '?category=' + category, httpOptions);
+  }
+
+  createOrUpdateOrder(order: Order) {
+    return this.http.post(Constants.createOrUpdateOrder, order, httpOptions);
   }
 }
